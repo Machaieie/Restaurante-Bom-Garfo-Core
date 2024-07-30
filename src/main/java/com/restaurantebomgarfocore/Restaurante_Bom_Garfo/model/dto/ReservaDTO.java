c378a857b315a09ac809684a9bca243937cb3226
Machaieie <machaieieedwin1@gmail.com>
2024-07-30 21:07:37 +0200

    ajustes em pagamentos e reservas

--- src/main/java/com/restaurantebomgarfocore/Restaurante_Bom_Garfo/model/dto/ReservaDTO.java
+++ src/main/java/com/restaurantebomgarfocore/Restaurante_Bom_Garfo/model/dto/ReservaDTO.java
@@ -1,11 +1,12 @@
 package com.restaurantebomgarfocore.Restaurante_Bom_Garfo.model.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record ReservaDTO(
     @NotBlank String date,
     @NotBlank String time,
-    @NotBlank int numberPeople,
+    @Min(1) int numberPeople,
     @NotBlank String observations,
     @NotBlank String firstName,
     @NotBlank String lastName,
